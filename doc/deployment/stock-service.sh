f252c918f6ec29541199d82630a6d597725a479f
wild <240440056@qq.com>
2019-01-24 21:14:15 +0800

    deployment

---
+++ doc/deployment/stock-service.sh
@@ -0,0 +1,21 @@
+```
+#!/bin/bash
+
+App='stock-service'
+ProjectHome='/home/vagrant/stock/'$App
+RunHome='/opt/stock'
+Version='0.0.1'
+Time=`date '+%Y%m%d%H%M%S'`
+
+cd $ProjectHome
+git pull
+gradle build --no-daemon -x test
+
+kill -9 $(ps -ef|grep java|awk '/'$App.jar'/{print $2}')
+
+cp -f $RunHome/$App.jar $RunHome/$App-$Time.jar
+cp -f $ProjectHome/build/libs/$App-$Version.jar $RunHome/$App.jar
+
+cd $RunHome
+nohup java -jar -Xms16m -Xmx32m -Xss1024K -XX:MetaspaceSize=16m -jar $App.jar > /dev/null 2>&1 &
+```
