76b306e73e9f1ca9c593dbce85730d32f4b5ebe1
bosspen1 <240440056@qq.com>
2022-06-16 21:58:43 +0800

    trade

--- stock-service/src/main/java/vip/linhs/stock/trategy/handle/GridStrategyHandler.java
+++ stock-service/src/main/java/vip/linhs/stock/trategy/handle/GridStrategyHandler.java
@@ -194,7 +194,7 @@ public class GridStrategyHandler extends BaseStrategyHandler<GridStrategyInput, 
         result.setStockCode(stockCode);
         result.setRelatedDealCode(dealCode);
         result.setZqmc(tradeRuleVo.getStockName());
-        result.setMarket(StockUtil.getStockMarket(result.getStockCode()));
+        result.setMarket(StockUtil.getStockMarket(stockCode));
         submitList.add(result);
     }
 
